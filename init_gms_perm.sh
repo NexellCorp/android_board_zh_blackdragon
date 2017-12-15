9df51e390b8e15f860214f47a4b59702a2b35627
Sungwoo Park <swpark@nexell.co.kr>
2017-12-15 13:52:17 +0900

    init: Add script to give permission to GMS
    This patch adds init_gms_perm.sh to give below permissions to
    com.google.android.gms process.
    - android.permission.ACCESS_FINE_LOCATION
    - android.permission.READ_EXTERNAL_STORAGE
    
    Change-Id: If1dd7c667829cc781bdf156ba5ce908d34a5fa21
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

---
+++ init_gms_perm.sh
@@ -0,0 +1,4 @@
+#!/system/bin/sh
+
+/system/bin/pm grant com.google.android.gms android.permission.ACCESS_FINE_LOCATION
+/system/bin/pm grant com.google.android.gms android.permission.READ_EXTERNAL_STORAGE
