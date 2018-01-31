2c40956da6c7f6fd36b78d19981d5a97ce2f0e72
Sungwoo Park <swpark@nexell.co.kr>
2018-01-31 18:12:00 +0900

    component: Add components needed by carlife
    This patch adds components needed by carlife.
    
    Change-Id: I31b561fe672007f2c120985b74bddff03ef289a0
    Signed-off-by: Sungwoo Park <swpark@nexell.co.kr>

--- device.mk
+++ device.mk
@@ -280,3 +280,48 @@ PRODUCT_COPY_FILES += \
 # vold check fs
 PRODUCT_PROPERTY_OVERRIDES += \
 	persist.vold.check_fs=0
+
+# carlife with android phone
+PRODUCT_PACKAGES += \
+	libbdcl \
+	libdiagnose_usb_bdcl \
+	bdcl
+
+# carlife with iphone
+PRODUCT_PACKAGES += \
+	libusb1.0 \
+	libcnary \
+	libplist \
+	libplist++ \
+	plist_cmp \
+	plist_test \
+	plist_util \
+	libusbmuxdcommon \
+	libusbmuxd \
+	iproxy \
+	libcrypto_openssl \
+	libimobilecommon \
+	libmobiledevice \
+	ideviceinfo \
+	idevicename \
+	idevicepair \
+	idevicesyslog \
+	idevice_id \
+	idevicebackup \
+	idevicebackup2 \
+	ideviceimagemounter \
+	idevicescreenshot \
+	ideviceenterrecovery \
+	idevicedate \
+	ideviceprovision \
+	idevicedebugserverproxy \
+	idevicediagnostics \
+	idevicedebug \
+	idevicenotificationproxy \
+	idevicecrashreport \
+	usbmuxd \
+	libzip \
+	ideviceinstaller
+
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/iproxy.sh:system/bin/iproxy.sh
